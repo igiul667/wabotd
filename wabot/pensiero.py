fa2140331ed390167a94edbf992521ec0d362e75
igiul667 <48596120+igiul667@users.noreply.github.com>
2022-06-28 10:40:52 +0200

    Add files via upload

--- wabot/pensiero.py
+++ wabot/pensiero.py
@@ -6,10 +6,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c","--content",type=str)
 parser.add_argument("-n","--name",type=str)
 parser.add_argument("-l","--lang",type=str)
+parser.add_argument("-m","--mode",type=str)
 args = parser.parse_args()
 
 wikipedia.set_lang(args.lang)
 result = wikipedia.summary(args.content, sentences = 2)
-
-tts = gTTS(text=result, lang=args.lang, slow=False)
-tts.save(args.name+".mp3")
+if args.mode == "voice":
+    tts = gTTS(text=result, lang=args.lang, slow=False)
+    tts.save(args.name+".mp3")
+else:
+    with open(args.name+".txt", 'w') as file:
+        file.write(result)
